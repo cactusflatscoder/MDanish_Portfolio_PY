b8b4a1cc9009617171d6f58200aa6fb3bf64a8c1
M.G <101354414+cactusflatscoder@users.noreply.github.com>
2023-07-20 11:35:02 -0600

    Update CSC515_Module1_PortfolioMilestone.py
    I cleaned up some unnecessary whitespace.

--- CSC515_Module1_PortfolioMilestone.py
+++ CSC515_Module1_PortfolioMilestone.py
@@ -3,19 +3,14 @@
 
 # In[1]:
 
-
 import cv2
 
-
 # In[2]:
-
 
 # Load the image
 image = cv2.imread('shutterstock93075775--250.jpg')
 
-
 # In[3]:
-
 
 try:
     cv2.imshow('Image', image)
@@ -23,9 +18,7 @@ try:
 except:
     print('There was an error, and the image did not load correctly.')
 
-
 # In[4]:
-
 
 try:
     output_path = 'C:/Users/mgdan/Downloads/M1PM_image_copy.jpg'
@@ -33,10 +26,3 @@ try:
     print('Image saved successfully.')
 except:
     print('There was an error, and the image did not save correctly.')
-
-
-# In[ ]:
-
-
-
-
